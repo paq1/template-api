79c54b189b1c5e4fd60afafe7a2b573eec50df88
paq1 <p.paquin1605@gmail.com>
2025-01-19 15:15:13 +0100

    chore: debug

---
+++ src/core/{{aggregate_name}}/data/events/mod.rs
@@ -0,0 +1,5 @@
+use chrono::{DateTime, Utc};
+
+use crate::core::{{aggregate_name}}::data::{{aggregate_name}}_data::{{aggregate_name | capitalize}}Data;
+use crate::models::{{aggregate_name}}::views::{{{aggregate_name | capitalize}}CreatedView, {{aggregate_name | capitalize}}DataView, {{aggregate_name | capitalize}}ViewEvent};
+use framework_cqrs_lib::cqrs::models::jsonapi::CanBeView;
